346aa3cf04da0942147ea46861050faf9a4b35b0
Hito 0/1 <hito94101@gmail.com>
2025-04-01 14:04:28 +0100

    Add files via upload

---
+++ jogodavelha.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <confio.h>
+#include "myheader/jogar.h"
+
+int main(void)
+{
+    char done;
+    char continuar = carry_on_play();
+    clearscreen();
+    int op = caller();
+    
+    switch (op)
+    {
+    case 1:
+        done = ' ';
+        ini_matrix();
+        do
+        {
+            display_matrix();
+            get_player_move();
+            //Verifica se há vencedor
+            done = check();
+            if (done != ' ')
+                break; //Há vencedor!
+            get_computer_move();
+            //Verifica se há vencedor
+            done = check();
+
+        } while (done = ' ');
+
+        if (done == 'X')
+            winner();
+
+        else
+            loser();
+        break;
+        
+    case 2:
+        done = ' ';
+        ini_matrix();
+        do
+        {
+            display_matrix();
+            get_player_move();
+            //Verifica se há vencedor
+            done = check();
+            if (done != ' ')
+                break; //Há vencedor!
+            get_match_player();
+            //Verifica se há vencedor
+            done = check();
+
+        } while (done = ' ');
+
+        if (done == 'X')
+            winner();
+        else if (done == 'V')
+            loser2();
+        break;
+        
+    case 3:
+        printf("Jogo encerrado!");
+        exit(1);
+        break;
+        
+        default:
+            printf("Opção inválida!");
+            break;
+    }
+    
+    return 0;
+}
